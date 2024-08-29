fdbfc5c27cc4114bcde750d9594681590a79cde6
josuetorres27 <josuetorres7260@gmail.com>
2024-08-28 19:05:17 -0600

    Acomodar algunos comentarios

--- ejemplos/pthreads/hello_w/src/hello_w.c
+++ ejemplos/pthreads/hello_w/src/hello_w.c
@@ -2,8 +2,8 @@
 
 /**
  * @file hello_w.c
- * @brief Se crean n hilos de ejecución, donde cada uno saluda e indica su 
- * rank, y al final se saluda desde el hilo principal.
+ * @brief Crea n cantidad de hilos secundarios, donde cada uno saluda e indica 
+ * su rank. Al final saluda desde el hilo principal.
  * 
  * @details El programa crea una cantidad arbitraria de hilos de ejecución. 
  * Cada hilo imprime "Hello from secondary thread i" en la salida estándar 
@@ -17,7 +17,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> /**< Permite utilizar características de la biblioteca estándar de Unix */
+#include <unistd.h> /**< Permite utilizar características de la biblioteca estándar de Unix. */
 
 void* greet(void* data);
 int create_threads(uint64_t thread_count);
@@ -26,38 +26,42 @@ int create_threads(uint64_t thread_count);
  * @brief Crea un arreglo con una cantidad de hilos secundarios que recibe por parámetro.
  * 
  * @param[in] argc Número de argumentos de línea de comandos.
- * @param[in] argv Array de cadenas de caracteres que representan los argumentos 
- *                 de línea de comandos.
- * @return Código de estado del programa
+ * @param[in] argv Arreglo de cadenas de caracteres que representan los argumentos 
+ * de línea de comandos.
+ * @return Código de estado del programa.
  */
-int main(int argc, char* argv[]) { /**< Se reciben los argumentos que se mandan por consola y crea un arreglo de caracteres */
+int main(int argc, char* argv[]) { /**< Se reciben los argumentos que se mandan por consola y crea un arreglo de caracteres. */
   int error = EXIT_SUCCESS;
-  uint64_t thread_count = sysconf(_SC_NPROCESSORS_ONLN); /**< Se crea un entero sin signo de 64 bits para especificar el espacio que se necesita*/
-  if (argc == 2) { /**< Se reciben dos argumentos, los cuales deben ser utilizados*/
-    if (sscanf(argv[1], "%" SCNu64, &thread_count) == 1) { /**< Se hace sscanf() para convertir texto (caracteres) a un entero de 64 bits, luego se guarda en thread_count*/
+  uint64_t thread_count = sysconf(_SC_NPROCESSORS_ONLN); /**< Entero sin signo de 64 bits para especificar el espacio que se necesita. */
+  if (argc == 2) {
+    if (sscanf(argv[1], "%" SCNu64, &thread_count) == 1) { /**< Se convierte el texto a un entero de 64 bits y se guarda en thread_count. */
     } else {
-      fprintf(stderr, "Error: invalid thread count\n"); /**< Se imprime si no se logra hacer el cast de antes*/
+      fprintf(stderr, "Error: invalid thread count\n"); /**< Se imprime si no se logra hacer el cast del texto. */
       return 11;
     }
   }
-  error = create_threads(thread_count); /**< error va a indicar si hubo algún error en alguna de las creaciones de los hilos*/
+  error = create_threads(thread_count); /**< Indica si hubo algún error en las creaciones de los hilos. */
   return error;
 }
 
 /**
- * @brief Crea una cantidad especificada de hilos
+ * @brief Crea una cantidad especificada de hilos.
  * 
  * @param[in] thread_count Número de hilos secundarios a crear.
- * @return Código de estado
+ * @return Código de estado.
  */
 int create_threads(uint64_t thread_count) {
   int error = EXIT_SUCCESS;
-  pthread_t* threads = (pthread_t*) malloc(thread_count * sizeof(pthread_t)); /**< Se crea un arreglo (puntero) de estructuras de tipo pthread_t, el cual llama al sistema operativo para solicitar memoria para cada una de las estructuras*/
+/**
+ * @brief Crea un arreglo (puntero) de estructuras de tipo pthread_t, el cual 
+ * llama al SO para solicitar memoria para cada una de las estructuras.
+ */
+  pthread_t* threads = (pthread_t*) malloc(thread_count * sizeof(pthread_t));
   if (threads) {
     for (uint64_t thread_number = 0; thread_number < thread_count
-        ; ++thread_number) { /**< Este for va recorriendo cada una de las estructuras pthread_t que se crearon en el arreglo*/
+        ; ++thread_number) { /**< Va recorriendo cada una de las estructuras pthread_t que se crearon en el arreglo. */
       error = pthread_create(&threads[thread_number], /*attr*/ NULL, greet
-        , /*arg*/ (void*) thread_number); /**< Para cada uno de los pthreads se hace un pthread_create() desde el hilo principal*/
+        , /*arg*/ (void*) thread_number); /**< Para cada uno de los pthread_t se hace un pthread_create() desde el hilo principal. */
       if (error == EXIT_SUCCESS) {
       } else {
         fprintf(stderr, "Error: could not create secondary thread\n");
@@ -65,15 +69,12 @@ int create_threads(uint64_t thread_count) {
         break;
       }
     }
-
-    printf("Hello from main thread\n"); /**< El hilo principal saluda una vez que se han creado todos los hilos secundarios*/
-
+    printf("Hello from main thread\n"); /**< El hilo principal saluda una vez que se han creado todos los hilos secundarios. */
     for (uint64_t thread_number = 0; thread_number < thread_count
         ; ++thread_number) {
-      pthread_join(threads[thread_number], /*value_ptr*/ NULL); /**< Se recorre la estructura threads nuevamente para hacer que el hilo principal espere a cada hilo secundario*/
+      pthread_join(threads[thread_number], /*value_ptr*/ NULL); /**< Se recorre el arreglo para hacer que el hilo principal espere a cada hilo secundario. */
     }
-
-    free(threads); /**< Se libera la memoria utilizada para el arreglo de hilos*/
+    free(threads); /**< Se libera la memoria utilizada para el arreglo de hilos. */
   } else {
     fprintf(stderr, "Error: could not allocate %" PRIu64 " threads\n"
       , thread_count);
@@ -83,13 +84,13 @@ int create_threads(uint64_t thread_count) {
 }
 
 /**
- * @brief Imprime el saludo de cada hilo secundario con su rank.
-
- * @param[in] data Puntero a un valor que representa el número identificador del hilo.
- * @return Siempre retorna NULL
+ * @brief Imprime el saludo de cada hilo secundario y su rank.
+ * 
+ * @param[in] data Puntero a un valor que representa el rank del hilo.
+ * @return Siempre retorna NULL.
  */
 void* greet(void* data) {
-  const uint64_t rank = (uint64_t) data; /**< rank es el número identificador de cada hilo, el cual es el argumento recibido a la hora de hacer pthread_create */
+  const uint64_t rank = (uint64_t) data; /**< rank es el número identificador de cada hilo, el cual es el argumento recibido a la hora de hacer pthread_create. */
   printf("Hello from secondary thread %" PRIu64 "\n", rank);
   return NULL;
 }
