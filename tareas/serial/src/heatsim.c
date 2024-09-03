eec1a21819aec70ef6a0a024a37b26cb239d1bae
josuetorres27 <josuetorres7260@gmail.com>
2024-09-02 22:19:10 -0600

    Finalizar comentarios con formato Doxygen.

--- tareas/serial/src/heatsim.c
+++ tareas/serial/src/heatsim.c
@@ -18,6 +18,9 @@
 #include <string.h>
 #include <time.h>
 
+/**
+ * @brief Especificar el tamaño máximo permitido para las rutas de archivos.
+ */
 #define MAX_PATH_LENGTH 260
 
 /**
@@ -116,19 +119,31 @@ void read_plate(const char* filepath, long long int rows, long long int cols,
  */
 void simulate(double** plate, int rows, int cols, double delta_t, double alpha,
   double h, double epsilon, int* k, time_t* time_seconds) {
+
+  /**
+   * @brief Inicialización de estructuras de datos.
+   */
   double max_delta;
   double** next_plate = (double**) malloc(rows * sizeof(double *));
   for (int i = 0; i < rows; i++) {
     next_plate[i] = (double*) malloc(cols * sizeof(double));
   }
 
-  *k = 0;
+  *k = 0; /** Declaración de contadores. */
   *time_seconds = 0;
 
+  /**
+   * @brief Algoritmo de simulación de calor.
+   */
   do {
     max_delta = 0.0;
     for (int i = 1; i < rows - 1; i++) {
       for (int j = 1; j < cols - 1; j++) {
+
+        /**
+         * @brief Aplica la fórmula para calcular la nueva temperatura de cada 
+         * celda.
+         */
         next_plate[i][j] = plate[i][j] + (((delta_t * alpha) / (h * h)) *
           (plate[i-1][j] + plate[i+1][j] + plate[i][j-1] + plate[i][j+1] -
             (4 * plate[i][j])));
@@ -140,16 +155,19 @@ void simulate(double** plate, int rows, int cols, double delta_t, double alpha,
       }
     }
 
+    /**
+     * @brief Copiar nuevas temperaturas a la matriz principal.
+     */
     for (int i = 1; i < rows - 1; i++) {
       for (int j = 1; j < cols - 1; j++) {
         plate[i][j] = next_plate[i][j];
       }
     }
 
-    (*k)++;
+    (*k)++; /** Incrementar contadores. */
     *time_seconds += delta_t;
 
-  } while (max_delta > epsilon);
+  } while (max_delta > epsilon); /** Condición de parada. */
 
   for (int i = 0; i < rows; i++) {
     free(next_plate[i]);
@@ -167,15 +185,23 @@ void simulate(double** plate, int rows, int cols, double delta_t, double alpha,
  */
 void write_plate(const char* filepath, long long int rows, long long int cols,
   double** plate) {
-  FILE* file = fopen(filepath, "wb");
+
+  FILE* file = fopen(filepath, "wb"); /** Apertura del archivo. */
   if (file == NULL) {
     perror("Error opening the output file");
     exit(EXIT_FAILURE);
   }
 
+  /**
+   * @brief Escritura de las dimensiones de la matriz.
+   */
   fwrite(&rows, sizeof(long long int), 1, file);
   fwrite(&cols, sizeof(long long int), 1, file);
 
+  /**
+   * @brief Escribe cada fila de la matriz de temperaturas en el archivo 
+   * binario.
+   */
   for (long long int i = 0; i < rows; i++) {
     fwrite(plate[i], sizeof(double), cols, file);
   }
@@ -197,18 +223,28 @@ void write_plate(const char* filepath, long long int rows, long long int cols,
 void create_report(const char* job_file, const char* plate_filename,
   double delta_t, double alpha, double h, double epsilon, int k,
     time_t time_seconds) {
+
+  /**
+   * @brief Generar el nombre del archivo de reporte.
+   */
   char report_filename[MAX_PATH_LENGTH];
   snprintf(report_filename, sizeof(report_filename), "%s.tsv", job_file);
 
+  /**
+   * @brief Apertura del archivo de reporte.
+   */
   FILE* report_file = fopen(report_filename, "a");
   if (report_file == NULL) {
     perror("Error opening the report file");
     exit(EXIT_FAILURE);
   }
 
-  char formatted_time[48];
+  char formatted_time[48]; /** Formatear el tiempo total de simulación. */
   format_time(time_seconds, formatted_time, sizeof(formatted_time));
 
+  /**
+   * @brief Escribir los datos en el archivo de reporte.
+   */
   fprintf(report_file, "%s\t%lf\t%lf\t%lf\t%lf\t%d\t%s\n", plate_filename,
     delta_t, alpha, h, epsilon, k, formatted_time);
 
@@ -227,16 +263,28 @@ void create_report(const char* job_file, const char* plate_filename,
  * @return Código de retorno del programa.
  */
 int main(int argc, char *argv[]) {
+  /**
+   * @brief Verifica que los argumentos proporcionados en la línea de comandos 
+   * sean correctos.
+   */
   if (argc < 2 || argc > 4) {
     fprintf(stderr, "Usage: %s <job file> [thread count] [file directory]\n",
       argv[0]);
     return EXIT_FAILURE;
   }
 
+  /**
+   * @brief Configura las variables y rutas necesarias, como el número de 
+   * hilos y el directorio de trabajo.
+   */
   const char* job_file = argv[1];
   int num_threads = (argc >= 3) ? atoi(argv[2]) : 1;
   const char* dir_prefix = (argc == 4) ? argv[3] : ".";
 
+  /**
+   * @brief Abre el archivo de trabajo especificado por el usuario, que 
+   * contiene las especificaciones para la simulación.
+   */
   FILE* file = fopen(job_file, "r");
   if (file == NULL) {
     perror("Error opening the job file");
@@ -247,8 +295,15 @@ int main(int argc, char *argv[]) {
   char plate_filename[MAX_PATH_LENGTH];
   double delta_t, alpha, h, epsilon;
 
+  /**
+   * @brief Bucle de lectura de configuración del archivo de trabajo.
+   */
   while (fscanf(file, "%s %lf %lf %lf %lf", plate_filename, &delta_t, &alpha,
     &h, &epsilon) == 5) {
+
+    /**
+     * @brief Verificar el tamaño de la ruta.
+     */
     int written = snprintf(filepath, sizeof(filepath), "%s/%s", dir_prefix,
       plate_filename);
 
@@ -261,25 +316,53 @@ int main(int argc, char *argv[]) {
     long long int rows, cols;
     read_dimensions(filepath, &rows, &cols);
 
+    /**
+     * @brief Reserva memoria para la matriz.
+     */
     double** plate = (double**) malloc(rows * sizeof(double *));
     for (int i = 0; i < rows; i++) {
       plate[i] = (double*) malloc(cols * sizeof(double));
     }
 
+    /**
+     * @brief Lectura de la matriz de temperaturas inicial.
+     */
     read_plate(filepath, rows, cols, plate);
 
+  /**
+   * @brief Realiza una simulación para cada lámina especificada en el archivo 
+   * de trabajo.
+   * 
+   * @details Para cada simulación realiza las siguientes acciones:
+   * - Lee las dimensiones y la matriz de temperaturas iniciales desde un 
+   * archivo binario.
+   * - Realiza la simulación de propagación de calor hasta que se alcance el 
+   * equilibrio térmico.
+   * - Genera un archivo de reporte con los resultados de la simulación.
+   * - Guarda el estado final de la matriz en un archivo binario.
+   */
     int k;
     time_t time_seconds;
     simulate(plate, rows, cols, delta_t, alpha, h, epsilon, &k, &time_seconds);
 
+    /**
+     * @brief Genera el reporte de la simulación.
+     */
     create_report(job_file, plate_filename, delta_t, alpha, h, epsilon, k,
       time_seconds);
 
+    /**
+     * @brief Construcción y configuración del nombre del archivo de salida.
+     */
     char output_filename[MAX_PATH_LENGTH];
     snprintf(output_filename, sizeof(output_filename), "plate%03d-%d.bin",
       atoi(&plate_filename[5]), k);
     write_plate(output_filename, rows, cols, plate);
 
+    /**
+     * @brief Cierra el archivo de trabajo y libera la memoria dinámica que 
+     * se ha utilizado durante la ejecución.
+     */
     for (int i = 0; i < rows; i++) {
       free(plate[i]);
     }
